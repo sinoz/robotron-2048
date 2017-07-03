c71ee5f49a06bd03540b095167e2730fd9d1946c
Sino <ilyas.alfan.b@gmail.com>
2017-07-03 14:02:41 +0200

    Updated Button

--- GameLogic/Scene/Button.cs
+++ GameLogic/Scene/Button.cs
@@ -4,13 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
 namespace GameLogic.Scene
 {
     /// <summary>
     /// TODO
     /// </summary>
-    class Button : Widget
+    public class Button : Widget
     {
-        // TODO
+        /// <summary>
+        /// TODO
+        /// </summary>
+        private int width, height;
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        private Action action;
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Button(int x, int y, int width, int height, Action action) : base(new Vector2(x, y))
+        {
+            this.width = width;
+            this.height = height;
+            this.action = action;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private bool intersectsWith(int x, int y)
+        {
+            return x > absolute.X && y > absolute.Y && x < absolute.X + width && y < absolute.Y + height;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            MouseState state = Mouse.GetState();
+
+            if (intersectsWith(state.Position.X, state.Position.Y))
+            {
+                action.Invoke();
+            }
+
+            base.Update(gameTime);
+        }
     }
 }
